94cc2c04426d58537e372a3a6f41dd97623c5447
Victoria-Sabetti <64561875+Victoria-Sabetti@users.noreply.github.com>
2022-03-23 13:11:23 -0700

    update final
    Update final

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
+const colors = ["dodgerblue", "fuchsia", "gainsboro", "khaki", "White"];
 
-//cover the screen
+
+var playing = false;
+function play_audio() {
+	if(playing) {
+		 document.getElementById("myAudio").pause();
+		 playing=false;
+	} else {		
+		document.getElementById("myAudio").play();
+		playing = true;
+	}
+}
+
+  //cover the screen
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -11,20 +24,27 @@ var catchers = [];
 var left;
 var right;
 
-var score = document.querySelector('#score');
+var lblScore = document.querySelector('#score');
+var lblLives = document.querySelector('#life');
+var lblLevel = document.querySelector('#level');
 var scoreNum = 0;
+var lifeNum = 3;
+lblLives.innerHTML = lifeNum.toString();
+var level = 1;
+const LEVEL_BALLS = 10;
 
 class Catcher {
+    static color = 'brown'
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.speed = 10;
+        this.speed = 20;
     }
 
     draw() {
-        c.fillStyle = 'brown';
+        c.fillStyle = Catcher.color;
         c.fillRect(this.x, this.y, this.width, this.height);
     }
 
@@ -51,9 +71,13 @@ class Falling {
     constructor(x, y, radius) {
         this.x = x;
         this.y = y;
-        this.radius = radius;
-        this.color = 'dodgerblue';
-        this.yVel = 3;
+        this.radius = radius
+        this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        //make sure color length is 7, otherwise it doesn't work
+        while (this.color.length != 7) {
+            this.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        }
+        this.yVel = level + 2;
     }
 
     draw() {
@@ -75,6 +99,7 @@ var create = setInterval(() => {
     //positioning the falling balls q2sec
     var x = Math.random() * ((canvas.width - 20) - 20) + 20;
     falling.push(new Falling(x, -10, 20));
+
 }, 2000);
 
 function animation() {
@@ -87,17 +112,30 @@ function animation() {
         if (falling[i].y + falling[i].radius > catchers[0].y &&
             catchers[0].x <= falling[i].x + falling[i].radius &&
             falling[i].x - falling[i].radius <= catchers[0].x + catchers[0].width) {
-
+            Catcher.color = falling[i].color;
             falling.splice(i, 1);
+            catchers.forEach((catchers) => {
+                catchers.update();
+            });
             scoreNum++;
-            score.innerHTML = scoreNum.toString();
+            lblScore.innerHTML = scoreNum.toString();
+            if (scoreNum % LEVEL_BALLS == 0) {
+                level++;
+                lblLevel.innerHTML = level.toString();
+            }
+
         } else if (falling[i].y - falling[i].radius > catchers[0].y) {
-            //falling.splice(i, 1);
+            lifeNum--;
+            lblLives.innerHTML = lifeNum.toString();
+            if (lifeNum == 0) {
+                //falling.splice(i, 1);
+                clearInterval(create);
+                lblScore.innerHTML += " GAME OVER!";
+                alert("GAME OVER!");
+            } else {
+                alert("You have lost 1 life!");
+            }
             falling.splice(0, falling.length);
-            //catchers.splice(0, falling.length);
-            clearInterval(create);
-            score.innerHTML += " GAME OVER!";
-            alert("GAME OVER!");
         }
     }
 
@@ -118,6 +156,7 @@ document.addEventListener('keydown', (event) => {
     if (event.key == 'ArrowRight') {
         right = true;
     }
+	
 });
 
 document.addEventListener('keyup', () => {
